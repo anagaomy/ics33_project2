1d8c80eef3a91b09e70246e8b2adfa475d6183c7
Ana Gao <gaomy@uci.edu>
2024-05-08 21:27:40 -0700

    Separate module for database events

---
+++ p2app/engine/database.py
@@ -0,0 +1,20 @@
+# database.py
+
+
+import sqlite3
+from p2app.events import *
+
+
+class DatabaseManager:
+    def __init__(self):
+        self._conn = None
+        self.path = None
+
+    def open_database(self, event):
+        try:
+            self.path = event.path()
+            self._conn = sqlite3.connect(self.path)
+            self._conn.execute("PRAGMA foreign_keys = ON;")
+            yield DatabaseOpenedEvent(self.path)
+        except sqlite3.Error as e:
+            yield DatabaseOpenFailedEvent(str(e))
